e40670707f28a11bbd0faff559937c0daf464363
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2023-01-05 19:20:03 -0300

    add grid system

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { ScreenClassProvider, setConfiguration } from 'react-grid-system';
+
+setConfiguration({
+  containerWidths: [540, 740, 960, 1140, 1250, 1540],
+  breakpoints: [576, 768, 992, 1200, 1280, 1600],
+});
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+      <ScreenClassProvider>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </ScreenClassProvider>
     </Html>
   )
 }
