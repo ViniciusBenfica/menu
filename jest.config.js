78bd5e691d825475b5a35dbffd0b911c326f6b91
Vinicius <vinicius.benfica.ruy@gmail.com>
2023-02-25 19:54:34 -0300

    add new config for test

--- jest.config.js
+++ jest.config.js
@@ -11,10 +11,11 @@ const customJestConfig = {
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
     '^@/components/(.*)$': '<rootDir>/components/$1',
-
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^@/service/(.*)$': '<rootDir>/service/$1',
+
   },
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'node',
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
